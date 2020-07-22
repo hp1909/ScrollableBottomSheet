199a008b62d6630228bebb11d60f836afed5f8bd
phuc.nguyen <phuc.nguyen4@tiki.vn>
2020-07-22 15:51:52 +0700

    Initial Commit

---
+++ ScrollableBottomSheet/ScrollableBottomSheet.h
@@ -0,0 +1,19 @@
+//
+//  ScrollableBottomSheet.h
+//  ScrollableBottomSheet
+//
+//  Created by Phuc Nguyen on 7/22/20.
+//  Copyright © 2020 phucnguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScrollableBottomSheet.
+FOUNDATION_EXPORT double ScrollableBottomSheetVersionNumber;
+
+//! Project version string for ScrollableBottomSheet.
+FOUNDATION_EXPORT const unsigned char ScrollableBottomSheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollableBottomSheet/PublicHeader.h>
+
+
